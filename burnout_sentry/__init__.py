0aab8c3ef173746d3451e5fcbee428df3fb86437
Agate <me@agate.blue>
2022-04-07 14:24:28 +0200

    Retrieve list of commits from one or more repositiories

--- burnout_sentry/__init__.py
+++ burnout_sentry/__init__.py
@@ -14,12 +14,18 @@ def cli():
 
 
 @cli.command("report")
-def report():
+@click.argument("repository", nargs=-1, required=True)
+def report(repository):
     """
     Extract data from the given repositories and display a report for
     each contributor.
+
+    Takes one or more repository path (URLs or paths to local directories).
     """
-    click.echo("Hello world")
+    click.echo(f"Getting commits from {len(repository)} repositories…")
+    commits = pydriller.Repository(list(repository)).traverse_commits()
+
+    click.echo(f"Extracting data from {len(list(commits))} commits…")
 
 
 if __name__ == "__main__":
